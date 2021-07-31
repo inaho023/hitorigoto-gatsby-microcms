efdde5342b427fef7ae9feed7078e66340d4b635
inaho023 <inaho.lx@inaho.space>
2021-08-01 00:46:43 +0900

    プロフィールページを作り忘れた

--- src/templates/{page.id}.jsx
+++ src/templates/{page.id}.jsx
@@ -2,8 +2,7 @@
 import React from 'react'
 
 // Gatsby
-import { useStaticQuery, graphql } from 'gatsby'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import { graphql } from 'gatsby'
 
 // 自作モジュール
 import Layout from '../components/Layout'
@@ -11,36 +10,35 @@ import Layout from '../components/Layout'
 // スタイルシート
 import * as styles from '../styles/{pade.id}.module.scss'
 
-// /pages/profile.js
-const page = props => {
-  // クエリー組立
-  var ql = `
-    {
-      microcmsPage(pageId: {eq: "profile"}) {
-        pageId
-        title
-        body
-        image {
-          url
-          width
-          height
-        }
+// クエリー実行
+export const query = graphql`
+  query ProfileQuery($id: String!) {
+    microcmsPage(pageId: { eq: $id }) {
+      pageId
+      title
+      datetime(formatString: "YYYYMMDD")
+      body
+      image {
+        url
+        width
+        height
       }
     }
-  `
-  // クエリー実行
-  const { data } = useStaticQuery(graphql(ql))
+  }
+`
+
+// /pages/profile.js
+const page = ({ data }) => {
   // ポジション
-  const sitePosition = props.data.microcmsPage.title
+  const sitePosition = data.microcmsPage.title
   // イメージ
-  const image = data.microcmsPage.image && getImage(data.microcmsPage.image.url)
   return (
     <Layout sitePosition={sitePosition}>
       <div className={styles.wrapper}>
         <div className={styles.title}>
           <h1>{data.microcmsPage.title && data.microcmsPage.title}</h1>
         </div>
-        <GatsbyImage className={styles.image} image={image} alt={data.microcmsPage.title} />
+        <img className={styles.image} src={data.microcmsPage.image.url} alt={data.microcmsPage.title} />
         <div className={styles.post} key={data.microcmsPage.pageId} dangerouslySetInnerHTML={{ __html: data.microcmsPage.body }} />
       </div>
     </Layout>
