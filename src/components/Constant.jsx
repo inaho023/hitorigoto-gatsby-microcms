81eb739a584679681b7ce71366c0f8108c393af4
inaho023 <inaho.lx@inaho.space>
2021-10-12 23:52:01 +0900

    画像パラメーターをWebP固定に変更

--- src/components/Constant.jsx
+++ src/components/Constant.jsx
@@ -1,12 +1,12 @@
 // 共通画像パラメーター
-export const THUMB_IMG_OPT_LIST = '?fit=crop&crop=faces&fm=auto&q=20&w=400&h=250' // 記事リスト用画像オプション
-export const THUMB_IMG_OPT_NAVI = '?fit=crop&crop=faces&fm=auto&q=20&w=128&h=128' // 前後記事用画像オプション
+export const THUMB_IMG_OPT_LIST = '?fit=crop&crop=faces&fm=webp&q=20&w=400&h=250' // 記事リスト用画像オプション
+export const THUMB_IMG_OPT_NAVI = '?fit=crop&crop=faces&fm=webp&q=20&w=128&h=128' // 前後記事用画像オプション
 // デスクトップ用画像パラメーター
-export const THUMB_IMG_OPT_DESKTOP_DETAIL = '?fit=crop&crop=faces&fm=auto&q=50&w=960&h=960' // 記事詳細用画像オプション
-export const THUMB_IMG_OPT_DESKTOP_GALLERY = '?fit=crop&crop=faces&fm=auto&q=20&w=480&h=480' // ギャラリー用画像オプション
+export const THUMB_IMG_OPT_DESKTOP_DETAIL = '?fit=crop&crop=faces&fm=webp&q=50&w=960&h=960' // 記事詳細用画像オプション
+export const THUMB_IMG_OPT_DESKTOP_GALLERY = '?fit=crop&crop=faces&fm=webp&q=20&w=480&h=480' // ギャラリー用画像オプション
 // モバイル用画像パラメーター
-export const THUMB_IMG_OPT_MOBILE_DETAIL = '?fit=crop&crop=faces&fm=auto&q=50&w=600&h=600' // 記事詳細用画像オプション
-export const THUMB_IMG_OPT_MOBILE_GALLERY = '?fit=crop&crop=faces&fm=auto&q=20&w=280&h=280' // ギャラリー用画像オプション
+export const THUMB_IMG_OPT_MOBILE_DETAIL = '?fit=crop&crop=faces&fm=webp&q=50&w=600&h=600' // 記事詳細用画像オプション
+export const THUMB_IMG_OPT_MOBILE_GALLERY = '?fit=crop&crop=faces&fm=webp&q=20&w=280&h=280' // ギャラリー用画像オプション
 // シェアボタン用設定
 export const SHARE_BUTTON_SIZE = 40 // シェアボタンサイズ
 // 外部リンク
