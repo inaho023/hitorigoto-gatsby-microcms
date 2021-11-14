5d1b9a11b023e4ded3c3fe6939dd37dd82c9dd06
inaho <inaho.lx@inaho.space>
2021-11-14 16:57:26 +0900

    プレビューページを作成

---
+++ src/pages/page/preview.jsx
@@ -0,0 +1,31 @@
+// React
+import React, { useEffect, useState } from 'react'
+
+// その他
+import queryString from 'query-string'
+
+// コンポーネント
+import BlogPost from '../../components/BlogPost'
+
+const PagePreview = ({ location }) => {
+  const { contentId, draftKey } = queryString.parse(location.search)
+  const [data, setData] = useState(null)
+
+  // 記事詳細取得
+  useEffect(() => {
+    fetch(`https://inaho.microcms.io/api/v1/blog/${contentId}?draftKey=${draftKey}`, {
+      headers: {
+        'X-MICROCMS-API-KEY': process.env.MICROCMS_API_KEY
+      }
+    })
+      .then(res => res.json())
+      .then(res => setData({ microcmsBlog: res }))
+  }, [])
+  // リターン
+  if (data === null) {
+    return null
+  }
+  return <BlogPost data={data} />
+}
+
+export default PagePreview
