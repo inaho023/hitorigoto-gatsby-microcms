d1bfecd3c175ab04e5b30568be9b4c1c4d4e53c6
inaho <inaho.lx@inaho.space>
2022-01-08 17:05:46 +0900

    メニューバーの構造を調整

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -37,38 +37,36 @@ const Menubar = () => {
   `)
   // リターン
   return (
-    <Box className={styles.wrapper}>
-      <AppBar className={styles.AppBar} position={'sticky'}>
-        <Toolbar>
-          <Link key={'Home'} to={'/'}>
-            <p>{data.site.siteMetadata.title + ' ' + data.site.siteMetadata.subtitle}</p>
-          </Link>
-          <Box className={styles.grow} />
-          <Box className={styles.icon}>
-            <IconButton color={'inherit'}>
-              <Link key={'Profile'} to={'/profile'}>
-                <Icon path={mdiAccount} size={1} title={'プロフィール'} />
-              </Link>
-            </IconButton>
-            <IconButton color={'inherit'}>
-              <a key={'Twitter'} href={URL_MY_TWITTER} target={'_blank'} rel={'noreferrer'}>
-                <Icon path={mdiTwitter} size={1} title={'Twitter'} />
-              </a>
-            </IconButton>
-            <IconButton color={'inherit'}>
-              <a key={'Instagram'} href={URL_MY_INSTAGRAM} target={'_blank'} rel={'noreferrer'}>
-                <Icon path={mdiInstagram} size={1} title={'Instagram'} />
-              </a>
-            </IconButton>
-            <IconButton color={'inherit'}>
-              <a key={'TwitCasting'} href={URL_MY_TWITCASTING} target={'_blank'} rel={'noreferrer'}>
-                <Icon path={mdiBroadcast} size={1} title={'ツイキャス'} />
-              </a>
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar className={styles.appbar} position={'sticky'}>
+      <Toolbar className={styles.toolbar}>
+        <Link key={'Home'} to={'/'}>
+          <p>{data.site.siteMetadata.title + ' ' + data.site.siteMetadata.subtitle}</p>
+        </Link>
+        <Box className={styles.grow} />
+        <Box className={styles.icon}>
+          <IconButton color={'inherit'}>
+            <Link key={'Profile'} to={'/profile'}>
+              <Icon path={mdiAccount} size={1} title={'プロフィール'} />
+            </Link>
+          </IconButton>
+          <IconButton color={'inherit'}>
+            <a key={'Twitter'} href={URL_MY_TWITTER} target={'_blank'} rel={'noreferrer'}>
+              <Icon path={mdiTwitter} size={1} title={'Twitter'} />
+            </a>
+          </IconButton>
+          <IconButton color={'inherit'}>
+            <a key={'Instagram'} href={URL_MY_INSTAGRAM} target={'_blank'} rel={'noreferrer'}>
+              <Icon path={mdiInstagram} size={1} title={'Instagram'} />
+            </a>
+          </IconButton>
+          <IconButton color={'inherit'}>
+            <a key={'TwitCasting'} href={URL_MY_TWITCASTING} target={'_blank'} rel={'noreferrer'}>
+              <Icon path={mdiBroadcast} size={1} title={'ツイキャス'} />
+            </a>
+          </IconButton>
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
