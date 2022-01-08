ca69d53406c5e10bc6d39fa6af67f4d4cf9b1627
inaho <inaho.lx@inaho.space>
2022-01-08 20:12:56 +0900

    404ページにpageContextが存在しないので手動で似たような値を構成

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,6 +1,9 @@
 // React
 import React from 'react'
 
+// Gatsby
+import { useStaticQuery, graphql } from 'gatsby'
+
 // 自作モジュール
 import Layout from '../components/Layout'
 
@@ -8,7 +11,32 @@ import Layout from '../components/Layout'
 import * as styles from '../styles/404.module.scss'
 
 // 404ページ
-const E404 = ({ pageContext }) => {
+const E404 = () => {
+  // クエリー実行
+  const data = useStaticQuery(graphql`
+    {
+      site {
+        siteMetadata {
+          siteUrl
+          title
+          subtitle
+          description
+          lang
+        }
+      }
+      microcmsPicture(pictureId: { eq: "ogp-no-picture" }) {
+        pictureId
+        title
+        picture {
+          url
+          width
+          height
+        }
+        parameter
+      }
+    }
+  `)
+  const pageContext = { info: { site: data.site.siteMetadata, image: data.microcmsPicture } }
   // OGP設定
   const ogp = {
     type: 'article',
@@ -18,7 +46,7 @@ const E404 = ({ pageContext }) => {
   const misc = { position: '404', ogp: ogp }
   // リターン
   return (
-    <Layout data={misc} pageContext={pageContext}>
+    <Layout misc={misc} pageContext={pageContext}>
       <div className={styles.post}>
         <h2>404 | ページがありません</h2>
       </div>
