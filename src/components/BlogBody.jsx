d50918cc49b7d4e26df860473b3bc2df813e3d8b
inaho <inaho.lx@inaho.space>
2022-03-29 14:44:01 +0900

    本文中の画像に透かしを追加

--- src/components/BlogBody.jsx
+++ src/components/BlogBody.jsx
@@ -8,21 +8,33 @@ import Paper from '@mui/material/Paper'
 import cheerio from 'cheerio'
 import hljs from 'highlight.js'
 
+// 自作モジュール
+import { imgixWatermark } from './Util'
+
 // スタイルシート
 import 'highlight.js/styles/base16/windows-10.css'
 import * as styles from '../styles/BlogBody.module.scss'
 
 // ブログ本文処理
 const BlogBody = ({ body }) => {
+  // ウォーターマーク生成
+  const imageWatermark = imgixWatermark()
   // シンタックスハイライト処理
-  const $ = cheerio.load(body)
-  $('pre code').each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text())
-    $(elm).html(result.value)
-    $(elm).addClass('hljs')
+  const cheerioBody = cheerio.load(body)
+  cheerioBody('pre code').each((_, elm) => {
+    const result = hljs.highlightAuto(cheerioBody(elm).text())
+    cheerioBody(elm).html(result.value)
+    cheerioBody(elm).addClass('hljs')
   })
+  // 画像に透かしを追加
+  cheerioBody('img').replaceWith((_, elm) => {
+    const src = cheerioBody(elm).attr('src')
+    const img = `${src}?fm=webp&q=50${imageWatermark.l}`
+    return cheerioBody(elm).attr('src', img)
+  })
+
   // リターン
-  return <Paper className={styles.post} key={'Post'} dangerouslySetInnerHTML={{ __html: $.html() }} />
+  return <Paper className={styles.post} key={'Post'} dangerouslySetInnerHTML={{ __html: cheerioBody.html() }} />
 }
 
 export default BlogBody
