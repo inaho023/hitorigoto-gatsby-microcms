a6c0eda31a05c4cf0a75866928d4282284416734
inaho <inaho.lx@inaho.space>
2022-04-08 23:13:10 +0900

    不要なクエリーを削除

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,9 +1,6 @@
 // React
 import React from 'react'
 
-// Gatsby
-import { useStaticQuery, graphql } from 'gatsby'
-
 // 自作モジュール
 import Layout from '../components/Layout'
 
@@ -12,31 +9,6 @@ import * as styles from '../styles/404.module.scss'
 
 // 404ページ
 const E404 = () => {
-  // クエリー実行
-  const data = useStaticQuery(graphql`
-    {
-      site {
-        siteMetadata {
-          siteUrl
-          title
-          subtitle
-          description
-          lang
-        }
-      }
-      microcmsPicture(pictureId: { eq: "ogp-no-picture" }) {
-        pictureId
-        title
-        picture {
-          url
-          width
-          height
-        }
-        parameter
-      }
-    }
-  `)
-  const pageContext = { info: { site: data.site.siteMetadata, image: data.microcmsPicture } }
   // OGP設定
   const ogp = {
     type: 'article',
@@ -47,7 +19,7 @@ const E404 = () => {
   const misc = { position: '404', ogp: ogp }
   // リターン
   return (
-    <Layout misc={misc} pageContext={pageContext}>
+    <Layout misc={misc}>
       <div className={styles.post}>
         <h2>404 | ページがありません</h2>
       </div>
