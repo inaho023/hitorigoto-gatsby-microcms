2bf447feee5eb297d02e562697faad351c11d962
inaho <inaho.lx@inaho.space>
2022-06-16 23:04:05 +0900

    ギャラリーのモジュール変更（PhotoSwipe→lightGallery）

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -12,7 +12,9 @@ import Icon from '@mdi/react'
 import { mdiChevronDown, mdiCamera } from '@mdi/js'
 
 // その他モジュール
-import { Gallery, Item } from 'react-photoswipe-gallery'
+import LightGallery from 'lightgallery/react'
+import lgZoom from 'lightgallery/plugins/zoom'
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
 
 // 自作ライブラリー
 import { imgixWatermark } from '../libs/Util'
@@ -20,7 +22,9 @@ import { imgixImageOption } from '../libs/Constant'
 
 // スタイルシート
 import * as styles from '../styles/PhotoGallery.module.scss'
-import 'photoswipe/dist/photoswipe.css'
+import 'lightgallery/scss/lightgallery.scss'
+import 'lightgallery/scss/lg-zoom.scss'
+import 'lightgallery/scss/lg-thumbnail.scss'
 
 // ギャラリーコンポーネント
 const PhotoGallery = ({ galleries }) => {
@@ -28,19 +32,41 @@ const PhotoGallery = ({ galleries }) => {
   const imageWatermark = imgixWatermark()
   // リターン
   return galleries.map(galleries => {
-    return galleries.gallery.map((gallery, index) => {
+    return galleries.gallery.map(gallery => {
+      // クラス名生成
+      const className = 'lg-' + gallery.id
       // ギャラリータイトル設定
       const galleryTitle = gallery.display_name ? gallery.display_name : gallery.name
       // リターン
       return (
-        <Accordion key={'Gallery' + (index + 1).toString()} className={styles.gallery} defaultExpanded>
+        <Accordion key={'Gallery-' + gallery.id} className={styles.gallery} defaultExpanded>
           <AccordionSummary className={styles.summary} expandIcon={<Icon path={mdiChevronDown} size={1} />}>
             <Icon path={mdiCamera} size={1} title={'ギャラリー'} />
             <span className={styles.text}>{galleryTitle}</span>
           </AccordionSummary>
-          <AccordionDetails>
-            <Gallery withCaption>
-              <Grid container id={gallery.id} className={styles.details} spacing={0}>
+          <AccordionDetails className={styles.detail}>
+            <LightGallery
+              licenseKey={process.env.LIGHTGALLERY_KEY}
+              plugins={[lgZoom, lgThumbnail]}
+              mode={'lg-fade'}
+              elementClassNames={styles.thumbnails}
+              selector={'.' + className}
+              mousewheel
+              strings={{
+                closeGallery: '閉じる',
+                toggleMaximize: '最大化',
+                previousSlide: '前へ',
+                nextSlide: '次へ',
+                download: 'ダウンロード',
+                playVideo: '再生'
+              }}
+              ZoomPluginStrings={{
+                zoomIn: 'ズームイン',
+                zoomOut: 'ズームアウト',
+                viewActualSize: '元のサイズ'
+              }}
+            >
+              <Grid container spacing={0}>
                 {
                   // 画像を配置
                   gallery.images.map((images, index) => {
@@ -52,8 +78,8 @@ const PhotoGallery = ({ galleries }) => {
                       (index + 1).toString() +
                       '枚目'
                     // 画像URL生成
-                    const original = images.image.url + '?' + imageWatermark.full
-                    const thumbnail = images.image.url + imgixImageOption.gallery.xl + imageWatermark.s
+                    const dataSrc = images.image.url + '?' + imageWatermark.full
+                    const dataSize = images.image.width.toString() + '-' + images.image.height.toString()
                     const src = images.image.url + imgixImageOption.gallery.xl + imageWatermark.m
                     const srcSet =
                       `${images.image.url}${imgixImageOption.gallery.xs}${imageWatermark.s} 280w,` +
@@ -64,28 +90,22 @@ const PhotoGallery = ({ galleries }) => {
                     const sizes = '(min-width:900px) 25vw, 50vw'
                     // リターン
                     return (
-                      <Item
+                      <Grid
+                        item
                         key={images.image.url}
-                        id={images.image.url}
-                        original={original}
-                        thumbnail={thumbnail}
-                        width={images.image.width}
-                        height={images.image.height}
-                        caption={title}
+                        className={className + ' ' + styles.thumbnail}
+                        xs={6}
+                        md={3}
+                        data-src={dataSrc}
+                        data-lg-size={dataSize}
                       >
-                        {({ ref, open }) => {
-                          return (
-                            <Grid item xs={6} md={3}>
-                              <img ref={ref} onClick={open} srcSet={srcSet} sizes={sizes} src={src} alt={title} />
-                            </Grid>
-                          )
-                        }}
-                      </Item>
+                        <img srcSet={srcSet} sizes={sizes} src={src} alt={title} />
+                      </Grid>
                     )
                   })
                 }
               </Grid>
-            </Gallery>
+            </LightGallery>
           </AccordionDetails>
         </Accordion>
       )
