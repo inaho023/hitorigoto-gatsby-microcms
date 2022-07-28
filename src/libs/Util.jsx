0a256cc2d38817362042bee2959e40fb2ef34051
inaho <inaho.lx@inaho.space>
2022-07-28 17:29:33 +0900

    画像処理でもとのwidth、heightを使用するように変更

--- src/libs/Util.jsx
+++ src/libs/Util.jsx
@@ -83,8 +83,6 @@ const imageProcessor = ({ cheerio, title, index }) => {
   const src = imgSrc + imgixImageOption.body.l + imageWatermark.l
   // 属性削除
   cheerio.removeAttr('src')
-  // cheerio.removeAttr('width')
-  // cheerio.removeAttr('height')
   // 属性設定
   cheerio.attr('srcSet', srcSet)
   cheerio.attr('sizes', sizes)
