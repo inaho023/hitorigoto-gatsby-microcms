28fd562e6a2292d585b823470ee0aff38ecce2f1
inaho <inaho.lx@inaho.space>
2022-08-02 15:51:24 +0900

    透かしを少し濃く変更

--- src/libs/Util.jsx
+++ src/libs/Util.jsx
@@ -39,12 +39,12 @@ export const imgixWatermark = () => {
     xs: Base64.encodeURI(`${data.microcmsPicture.picture.url}?w=50&h=10&fm=png${data.microcmsPicture.parameter}`)
   }
   return {
-    full: `&mark-w=300&mark-h=60&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.full}`,
-    xl: `&mark-w=250&mark-h=50&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.xl}`,
-    l: `&mark-w=200&mark-h=40&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.l}`,
-    m: `&mark-w=150&mark-h=30&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.m}`,
-    s: `&mark-w=100&mark-h=20&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.s}`,
-    xs: `&mark-w=50&mark-h=10&mark-align=bottom,center&mark-alpha=30&mark64=${base64Image.xs}`
+    full: `&mark-w=300&mark-h=60&mark-align=bottom,center&mark-alpha40&mark64=${base64Image.full}`,
+    xl: `&mark-w=250&mark-h=50&mark-align=bottom,center&mark-alpha=40&mark64=${base64Image.xl}`,
+    l: `&mark-w=200&mark-h=40&mark-align=bottom,center&mark-alpha=40&mark64=${base64Image.l}`,
+    m: `&mark-w=150&mark-h=30&mark-align=bottom,center&mark-alpha=40&mark64=${base64Image.m}`,
+    s: `&mark-w=100&mark-h=20&mark-align=bottom,center&mark-alpha=40&mark64=${base64Image.s}`,
+    xs: `&mark-w=50&mark-h=10&mark-align=bottom,center&mark-alpha=40&mark64=${base64Image.xs}`
   }
 }
 
