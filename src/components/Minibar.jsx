e89f7058ffe5a4be33508203ab2874543182ca94
inaho <inaho.lx@inaho.space>
2022-08-09 22:43:51 +0900

    パンくずリスト削除と作り直し

--- src/components/Minibar.jsx
+++ src/components/Minibar.jsx
@@ -3,7 +3,6 @@ import React from 'react'
 
 // Gatsby
 import { Link } from 'gatsby'
-import { Breadcrumb } from 'gatsby-plugin-breadcrumb'
 
 // Material-UI
 import Box from '@mui/material/Box'
@@ -11,103 +10,105 @@ import IconButton from '@mui/material/IconButton'
 
 // Material Design Icons
 import Icon from '@mdi/react'
-import { mdiPageFirst, mdiChevronLeft, mdiChevronRight, mdiPageLast } from '@mdi/js'
+import {
+  mdiHome,
+  mdiCalendarToday,
+  mdiShape,
+  mdiTag,
+  mdiPageFirst,
+  mdiChevronLeft,
+  mdiChevronRight,
+  mdiPageLast
+} from '@mdi/js'
 
 // スタイルシート
 import * as styles from '../styles/Minibar.module.scss'
 
 // ミニバーコンポーネント
-const Minibar = ({ misc, pageContext }) => {
-  // pageContextがNULLかUndefinedの場合はリターン
-  if (!pageContext) {
-    return null
+const Minibar = ({ pageContext }) => {
+  // リストアイコン設定
+  let listIcon
+  switch (pageContext.list) {
+    case 'archive':
+      // アイコン
+      listIcon = mdiCalendarToday
+      break
+    case 'category':
+      // アイコン
+      listIcon = mdiShape
+      break
+    case 'tag':
+      // アイコン
+      listIcon = mdiTag
+      break
+    default:
+      // アイコン
+      listIcon = mdiHome
+      break
   }
-  // パンくずリスト
-  const disableLinks = ['/archive', '/category', '/tag']
-  const hiddenCrumbs = ['/page', '/post']
-  // パス設定
-  let nav = {}
-  if (misc.ogpInfo.type === 'website') {
-    nav = {
-      first: {
-        path: pageContext.list === 'all' ? '/' : `/${pageContext.list}/${pageContext.id}/`,
-        title: '最初のページへ',
-        disabled: pageContext.humanPageNumber === 1
-      },
-      prev: {
-        path: pageContext.previousPagePath,
-        title: '前のページへ',
-        disabled: pageContext.humanPageNumber === 1
-      },
-      next: {
-        path: pageContext.nextPagePath,
-        title: '次のページへ',
-        disabled: pageContext.humanPageNumber === pageContext.numberOfPages
-      },
-      last: {
-        path:
-          pageContext.list === 'all'
-            ? `/page/${pageContext.numberOfPages.toString()}/`
-            : `/${pageContext.list}/${pageContext.id}/${pageContext.numberOfPages.toString()}/`,
-        title: '最後のページへ',
-        disabled: pageContext.humanPageNumber === pageContext.numberOfPages
-      }
-    }
-  } else {
-    nav = {
-      first: {
-        path: pageContext.first ? `/post/${pageContext.first.node.blogId}` : '/',
-        title: pageContext.first ? pageContext.first.node.title : '',
-        disabled: !pageContext.first || pageContext.id === pageContext?.first?.node?.blogId
-      },
-      prev: {
-        path: pageContext.prev ? `/post/${pageContext.prev.node.blogId}` : '/',
-        title: pageContext.prev ? pageContext.prev.node.title : '',
-        disabled: pageContext.prev ? false : true
-      },
-      next: {
-        path: pageContext.next ? `/post/${pageContext.next.node.blogId}` : '/',
-        title: pageContext.next ? pageContext.next.node.title : '',
-        disabled: pageContext.next ? false : true
-      },
-      last: {
-        path: pageContext.last ? `/post/${pageContext.last.node.blogId}` : '/',
-        title: pageContext.last ? pageContext.last.node.title : '',
-        disabled: !pageContext.last || pageContext.id === pageContext?.last?.node?.blogId
-      }
+  // ナビゲーションパス設定
+  const nav = {
+    first: {
+      path: pageContext.list === 'all' ? '/' : `/${pageContext.list}/${pageContext.id}/`,
+      title: '最初のページへ',
+      disabled: pageContext.humanPageNumber === 1
+    },
+    prev: {
+      path: pageContext.previousPagePath,
+      title: '前のページへ',
+      disabled: pageContext.humanPageNumber === 1
+    },
+    next: {
+      path: pageContext.nextPagePath,
+      title: '次のページへ',
+      disabled: pageContext.humanPageNumber === pageContext.numberOfPages
+    },
+    last: {
+      path:
+        pageContext.list === 'all'
+          ? `/page/${pageContext.numberOfPages.toString()}/`
+          : `/${pageContext.list}/${pageContext.id}/${pageContext.numberOfPages.toString()}/`,
+      title: '最後のページへ',
+      disabled: pageContext.humanPageNumber === pageContext.numberOfPages
     }
   }
   // リターン
   return (
     <Box className={styles.minibar}>
-      <Breadcrumb
-        crumbs={pageContext.breadcrumb.crumbs}
-        crumbLabel={misc.crumbLabel}
-        disableLinks={disableLinks}
-        hiddenCrumbs={hiddenCrumbs}
-      />
-      <Box className={styles.pager}>
-        <IconButton className={styles.button} disabled={nav.first.disabled}>
-          <Link key={'First'} to={nav.first.path}>
-            <Icon path={mdiPageFirst} size={2} title={nav.first.title} />
-          </Link>
-        </IconButton>
-        <IconButton className={styles.button} disabled={nav.prev.disabled}>
-          <Link key={'Prev'} to={nav.prev.path}>
-            <Icon path={mdiChevronLeft} size={2} title={nav.prev.title} />
-          </Link>
-        </IconButton>
-        <IconButton className={styles.button} disabled={nav.next.disabled}>
-          <Link key={'Next'} to={nav.next.path}>
-            <Icon path={mdiChevronRight} size={2} title={nav.next.title} />
-          </Link>
-        </IconButton>
-        <IconButton className={styles.button} disabled={nav.last.disabled}>
-          <Link key={'Last'} to={nav.last.path}>
-            <Icon path={mdiPageLast} size={2} title={nav.last.title} />
-          </Link>
-        </IconButton>
-      </Box>
+      <span className={styles.list}>
+        <span className={styles.wrapper}>
+          <span className={styles.icon}>
+            <Icon path={listIcon} size={2} />
+          </span>
+          <span className={styles.text}>
+            <h3>{pageContext.name ? pageContext.name : 'ホーム'}</h3>
+          </span>
+        </span>
+      </span>
+      <span className={styles.pager}>
+        <span className={styles.wrapper}>
+          <IconButton disabled={nav.first.disabled}>
+            <Link key={'First'} to={nav.first.path}>
+              <Icon path={mdiPageFirst} size={2} title={nav.first.title} />
+            </Link>
+          </IconButton>
+          <IconButton disabled={nav.prev.disabled}>
+            <Link key={'Prev'} to={nav.prev.path}>
+              <Icon path={mdiChevronLeft} size={2} title={nav.prev.title} />
+            </Link>
+          </IconButton>
+          <IconButton disabled={nav.next.disabled}>
+            <Link key={'Next'} to={nav.next.path}>
+              <Icon path={mdiChevronRight} size={2} title={nav.next.title} />
+            </Link>
+          </IconButton>
+          <IconButton disabled={nav.last.disabled}>
+            <Link key={'Last'} to={nav.last.path}>
+              <Icon path={mdiPageLast} size={2} title={nav.last.title} />
+            </Link>
+          </IconButton>
+        </span>
+      </span>
     </Box>
   )
 }
