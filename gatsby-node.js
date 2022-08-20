18d18691f406a4fd5033785458d8e29d5d6c127a
inaho <inaho.lx@inaho.space>
2022-08-20 21:22:04 +0900

    mini-css-extract-pluginの警告に対応

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,6 +5,17 @@ const { paginate } = require('gatsby-awesome-pagination')
 // 定数
 const siteListPerPage = 12 // 1ページあたりの記事数
 //
+exports.onCreateWebpackConfig = helper => {
+  const { stage, actions, getConfig } = helper
+  if (stage === 'develop') {
+    const config = getConfig()
+    const miniCssExtractPlugin = config.plugins.find(plugin => plugin.constructor.name === 'MiniCssExtractPlugin')
+    if (miniCssExtractPlugin) {
+      miniCssExtractPlugin.options.ignoreOrder = true
+    }
+    actions.replaceWebpackConfig(config)
+  }
+} //
 exports.createPages = async ({ graphql, actions }) => {
   // 定数定義
   const { createPage } = actions
